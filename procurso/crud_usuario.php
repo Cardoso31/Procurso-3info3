f7eb103b4a3413bca49773198a7a26b5d7c4e14c
Cardoso <32268902+Cardoso31@users.noreply.github.com>
2019-06-05 09:20:08 -0300

    Add files via upload

---
+++ procurso/crud_usuario.php
@@ -0,0 +1,25 @@
+<?php
+include 'header.php';
+
+if(isset($_GET['acao'])){
+	if(!empty($_GET['acao'])){
+
+		switch ($_GET['acao']) {
+			case 'login':
+								
+?>
+
+<?php			
+
+				break;
+			
+		case 'signin':
+?>
+
+
+<?php
+				break;
+		}
+
+	}
+}
---
+++ procurso/functions.php
@@ -0,0 +1,18 @@
+<?php
+
+class Usuario{
+	private $id_usuario;
+	public $nome;
+	public $sobrenome;
+	public $email;
+	public $datanasc;
+	public $senha;
+	public $tipo_usuario;
+	public $ftperfil;
+
+	public function login(){
+		
+	}
+
+}
+
